720c7d638f1409dbe5bf5e042900318f9864a651
paikwiki <paikwiki@gmail.com>
2018-01-15 00:43:54 +0900

    Creave navigation component👳🏽‍♀️
    Nav component is added on app, but it has unsuitable usage.
    Navigation links are hard code☹️

--- tiny-vue-cli-app/src/App.vue
+++ tiny-vue-cli-app/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
     <h1>Tiny Vue-cli App</h1>
+    <Nav/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Nav from './components/Nav';
+
 export default {
   name: 'App',
+  components: {
+    Nav
+  }
 };
 </script>
 
---
+++ tiny-vue-cli-app/src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <li><a :href="main">Main😎</a></li>
+      <li><a :href="rightHere">Where?🤔</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data() {
+    return {
+      main: '#/',
+      rightHere: '#/right-here'
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
